b83cebab2d01532435163975974d0be5fdf7d738
saddexed <seta5869@gmail.com>
2024-09-10 17:41:26 +0530

    Code cleanup

--- switchRR.cmd
+++ switchRR.cmd
@@ -1,37 +1,37 @@
-@echo off
+@ECHO off
 
 REM Find the current refresh rate and set it to the variable
 FOR /f "skip=2 tokens=5 delims= " %%a in ('qres /s') do SET rate=%%a
 
-REM Check if %1 is not null
-if "%1" == "" goto :min_max
+REM Check if %1 is null
+IF "%1" == "" goto :min_max
+
 REM Check if %1 is a valid refresh rate
-if "%1" == "60" goto :set_rate
-if "%1" == "75" goto :set_rate
-if "%1" == "90" goto :set_rate
-if "%1" == "120" goto :set_rate
-if "%1" == "144" goto :set_rate
+IF "%1" == "60" goto :set_rate
+IF "%1" == "75" goto :set_rate
+IF "%1" == "90" goto :set_rate
+IF "%1" == "120" goto :set_rate
+IF "%1" == "144" goto :set_rate
+
 REM If %1 is not a valid refresh rate, then exit
-echo %1 is an Invalid option
+ECHO %1 is an Invalid option
 goto :EOF
 
 REM If no arguments are passed, then toggle between 60 and 144
 :min_max
-if "%rate%" == "60" (
-    qres /r:144 > nul
-    set newrate=144
-    goto :end
-) else (
-    qres /r:60 > nul
-    set newrate=60
-    goto :end
+IF "%rate%" == "60" (
+    SET newrate=144
+) ELSE (
+    SET newrate=60
 )
+qres /r:%newrate% > nul
+goto :end
 
 :set_rate
-qres /r:%1 > nul
-set newrate=%1
+SET newrate=%1
+qres /r:%newrate% > nul
 goto :end
 
 :end
-powershell -File "C:\Programs\Qres\notifyRRchange.ps1" %rate% %newrate%
-echo Refresh rate changed from %rate% to %newrate%
+powershell -File "notifyRRchange.ps1" %rate% %newrate%
+ECHO Refresh rate changed from %rate% to %newrate%
